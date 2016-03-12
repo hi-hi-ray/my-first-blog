949e18870da4a46a505d4159ccd76b9cdc3193ee
rai-dietra4 <rai.dietra4@yahoo.com>
2016-03-12 17:13:17 -0300

    WSGI.PY

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,28 @@
-"""
-WSGI config for mysite project.
+# """
+# WSGI config for mysite project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+# """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+# application = get_wsgi_application()
 import os
+import sys
+
+path = '/home/rai-dietra4/my-first-blog'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
